b909e41c1f285e250b6c412a1f4b9400aa6dfb55
Ansh Karwasra <anshkarwasra15@gmail.com>
2024-09-02 19:07:09 +0530

    bug_fix: fixes the car price bug

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from datetime import datetime
 from joblib import load
 from werkzeug.utils import secure_filename
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-
+import pickle
 app = Flask(__name__)
-app.config['UPLOADER_FOLDER'] = "D:\\Programming\\hackathon project\\HPCopyZip\\HPCopy\\model_images"
+app.config['UPLOADER_FOLDER'] = "C:\\Users\\anshk\\Downloads\\Programming\\hackathon project\\HPCopyZip\\HPCopy\\model_images"
 
 import google.generativeai as genai
 import os
@@ -17,21 +16,22 @@ import os
 # AIzaSyBkpmK74q4CeYIlUmSX7TXEd76S86ISExA
 # set API_KEY=<AIzaSyBkpmK74q4CeYIlUmSX7TXEd76S86ISExA>
 
-genai.configure(api_key='your api key here')
+genai.configure(api_key='AIzaSyBkpmK74q4CeYIlUmSX7TXEd76S86ISExA')
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 regressor = load("model.joblib")
 
 def preProcess(row):
-    print(row)
-    le = LabelEncoder()
-    row['Car Brand'] = le.fit_transform([row['Car Brand']])
-    row['Car Model']=  le.fit_transform([row['Car Model']])
-    my_pipline = Pipeline(steps=[('scaler', StandardScaler())])
-    print(row)
-    row = my_pipline.fit_transform([row])
-    print("done")
+    with open('le_brand.pickle','rb') as f:
+        le_brand = pickle.load(f)
+    with open('le_model.pickle','rb') as f:
+        le_model = pickle.load(f)
+    with open('scaler.pickle','rb') as f:
+        scaler = pickle.load(f)
+    row["Car Brand"] = le_brand.transform([row["Car Brand"]])
+    row["Car Model"] = le_model.transform([row["Car Model"]])
+    row = scaler.transform([row])
     print(row)
     return row
 
@@ -49,22 +49,23 @@ def textgen():
 def uploader():
     # Upload the file and print a confirmation.
 
-# Prompt the model with text and the previously uploaded image.
+    # Prompt the model with text and the previously uploaded image.
     response = "No response yet."
     if request.method == "POST":
         
         f = request.files['file1']
         f.save(os.path.join(app.config['UPLOADER_FOLDER'], secure_filename(f.filename)))
         query = request.form.get("user_query")
-        sample_file = genai.upload_file(path=f"model_images\{f.filename}",
+        sample_file = genai.upload_file(path=f"C:\\Users\\anshk\\Downloads\\Programming\\hackathon project\\HPCopyZip\\HPCopy\\model_images\\{f.filename}",
                             display_name="an image")
         
-        response = model.generate_content(sample_file, query)
-        print(response.text)
-    try:
-        return render_template('image.html', response = response.text)
-    except Exception as e:
-        return render_template('image.html',response=response)
+        try:
+            response = model.generate_content(sample_file, query)
+            print(response.text)
+            return render_template('image.html', response = response.text)
+        except Exception as e:
+            return render_template('image.html',response="opps something went wrong")
+    return render_template('image.html',response=response)
 
 @app.route('/')
 def route():
@@ -95,14 +96,13 @@ def caarprice():
         carModel = request.form.get("car_model")
         df = pd.read_csv("data.csv")
         df = df.drop("Unnamed: 0",axis=1)
-        # try:
-        row = df[df['Car Brand'] == carBrand]
-        row = row[row['Car Model']== carModel]
-        print(row.iloc[0])
-        response = round(regressor.predict(preProcess(row.iloc[0]))[0] /10)
-        # except Exception as e:
-        #     print("sorry we don't have data for that car")
-        #     print(e)
+        try:
+            row = df[df['Car Brand'] == carBrand]
+            row = row[row['Car Model']== carModel]
+            response = round(regressor.predict(preProcess(row.iloc[0]))[0] /10)
+        except Exception as e:
+            response = "sorry we don't have data for that car"
+            print(e)
 
     return render_template("model.html",response=response)
 
